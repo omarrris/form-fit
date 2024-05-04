8fa3584fcffe53de23370c4e2804de345a325c8c
Omar Sheikh <omarr.sheikh@gmail.com>
2024-05-04 19:00:32 -0400

    Initial Commit

---
+++ formfit/formfitApp.swift
@@ -0,0 +1,17 @@
+//
+//  formfitApp.swift
+//  formfit
+//
+//  Created by Omar Sheikh on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct formfitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
